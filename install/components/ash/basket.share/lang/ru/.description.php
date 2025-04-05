5e61e1a85acc6abfead9c4c3aeb06184d9b92b16
Serg None <50916024+ash1133@users.noreply.github.com>
2025-04-05 21:58:49 +0300

    Update .description.php

--- install/components/ash/basket.share/lang/ru/.description.php
+++ install/components/ash/basket.share/lang/ru/.description.php
@@ -1,5 +1,5 @@
 <?php
 $MESS['BASKET_SHARE_COMPONENT_NAME'] = 'Поделиться корзиной';
 $MESS['BASKET_SHARE_COMPONENT_DESCRIPTION'] = 'Позволяет пользователям делиться содержимым своей корзины через короткую ссылку';
-$MESS['BASKET_SHARE_VENDOR_NAME'] = 'Моя компания';
+$MESS['BASKET_SHARE_VENDOR_NAME'] = 'Ash';
 $MESS['BASKET_SHARE_CATEGORY_NAME'] = 'Корзина';
